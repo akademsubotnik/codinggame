cfe088fed2a8baedd00f6ad2fa540d7ccc7593c2
Greg <44036625+akademsubotnik@users.noreply.github.com>
2023-05-28 19:40:49 +0300

    Update thedescent.py

--- python/thedescent.py
+++ python/thedescent.py
@@ -7,30 +7,35 @@ import math
 # and where you have to print an output (the index of the mountain to fire on)
 # The inputs you are given are automatically updated according to your last actions.
 
-mountainHeights = []
-mountainHeightsSorted = []
+mh = {}
+smh = {}
+listL = []
 
-def sortMountainHeights():
-    print("Sort")
+def add_element(dict, key, value):
+    if key not in dict:
+        dict[key] = []
+    dict[key].append(value)
 
-def shootMountains():
-    print("Shoot")
+def sort(dict):
+    #print(dict)
+    smh = sorted(dict.items(),reverse=True)
+    print(smh)
 
-    
+def fire(dict):
+    for values in smh.values():
+        print(values)
+
 # game loop
 while True:
     for i in range(8):
         mountain_h = int(input())  # represents the height of one mountain.
-        mountainHeights.append(mountain_h)
-
-    #print("MountainHeight List :" + str(mountainHeights))
-    mountainHeightsSorted = sorted(mountainHeights,reverse=True)
-    print(mountainHeightsSorted)
-
+        add_element(mh,mountain_h,i)
+    sort(mh)
+    #print(smh)
 
     # Write an action using print
     # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
     # The index of the mountain to fire on.
+    
     print("4")
-
