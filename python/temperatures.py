9a98d7917d4881bb50fa46b19e8418be20615b8a
Greg <44036625+akademsubotnik@users.noreply.github.com>
2023-05-29 00:35:08 +0300

    Update temperatures.py

--- python/temperatures.py
+++ python/temperatures.py
@@ -2,7 +2,8 @@
 #If two numbers are equally close to zero, positive integer has to be considered closest to zero 
 #(for instance, if the temperatures are -5 and 5, then display 5).
 
-#Passes 1/4 test cases - checking if no temperature is passed
+#Passes 5/5 test cases 
+#Translated from previous bash correct answer by AI you.com
 
 import sys
 import math
@@ -28,7 +29,34 @@ for i in input().split():
 #Handle Empty Array
 if len(myList) == 0 :
     print(closestToZero)
+    exit()
 else:
     closestToZero=myList[0]
 
+for i in myList:
+    # Handle i == -i
+    if i == closestToZero * -1:
+        if i < 0:
+            continue
+        else:
+            closestToZero = i
+
+    # Handle i < 0
+    if i < 0:
+        if closestToZero < 0:
+            if closestToZero > i:
+                continue
+            else:
+                closestToZero = i
+        else:
+            i = i * -1
+            if i > closestToZero:
+                continue
+            else:
+                closestToZero = i * -1
+    #Handle i greater than 0
+    if i  < closestToZero:
+        closestToZero=i
+
+print(closestToZero)
 #print("result")
