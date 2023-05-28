1f8ef5d9f7092f3fe2df9101adc038c37ed76325
Greg <44036625+akademsubotnik@users.noreply.github.com>
2023-05-28 22:13:50 +0300

    Update thedescent.py

--- python/thedescent.py
+++ python/thedescent.py
@@ -1,3 +1,7 @@
+#This code passes tests 2/5, it assumes that the mountains will fall after being shot 1x.  The other 3/5 cases a mountain requires 
+#multiple shots to fall
+#Bubblesort written by AI you.com
+
 import sys
 import math
 
@@ -16,21 +20,35 @@ def add_element(dict, key, value):
         dict[key] = []
     dict[key].append(value)
 
-def sort(dict):
-    #print(dict)
-    smh = sorted(dict.items(),reverse=True)
-    print(smh)
+def bubble_sort_dict(my_dict):
+    # Convert dictionary to list of tuples
+    my_list = list(my_dict.items())
 
-def fire(dict):
-    for values in smh.values():
-        print(values)
+    # Sort list using bubble sort
+    n = len(my_list)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if my_list[j][1] < my_list[j+1][1]:
+                my_list[j], my_list[j+1] = my_list[j+1], my_list[j]
+
+    # Convert sorted list back to dictionary
+    sorted_dict = {}
+    for item in my_list:
+        sorted_dict[item[0]] = item[1]
+
+    #print(sorted_dict)
+
+    for k in sorted_dict:
+        print(k)
+    #return sorted_dict
+
 
 # game loop
 while True:
     for i in range(8):
         mountain_h = int(input())  # represents the height of one mountain.
-        add_element(mh,mountain_h,i)
-    sort(mh)
+        add_element(mh,i,mountain_h)
+    bubble_sort_dict(mh)
     #print(smh)
 
     # Write an action using print
